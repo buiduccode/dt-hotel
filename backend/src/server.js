3bf017d4065ea6048aaedc4628a9a142b921085c
Bui Trong Duc <trongduc032005@gmail.com>
2026-06-01 00:48:06 +0700

    fix server.js serve frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const path = require('path');
 const app = require('./app');
 const { connectDB } = require('./config/db');
 
@@ -7,12 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, '../../front-end')));
+app.use('/front-end', require('express').static(path.join(__dirname, '../../front-end')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../front-end/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
