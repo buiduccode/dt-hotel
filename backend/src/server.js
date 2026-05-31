30250073418f2a356240cb159ebd98c7f2aa5ec0
Bui Trong Duc <trongduc032005@gmail.com>
2026-06-01 00:38:14 +0700

    serve frontend static files

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,3 +10,9 @@ connectDB();
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../../front-end')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../front-end/index.html'));
+});
